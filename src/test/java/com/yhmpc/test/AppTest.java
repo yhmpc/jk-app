088093904187090185767548126e7461b0cbb8c0
steve <yhmpc@qq.com>
2019-09-15 21:39:23 +0800

    modified the test 222

--- src/test/java/com/yhmpc/test/AppTest.java
+++ src/test/java/com/yhmpc/test/AppTest.java
@@ -6,6 +6,6 @@ public class AppTest {
 
     @Test
     public void mytest(){
-        System.out.println("This is JUnit 4.12 Test...1111");
+        System.out.println("This is JUnit 4.12 Test...1111 222");
     }
 }
